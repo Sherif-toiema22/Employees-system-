7727db6352cb4a51a2b7b1cc7586683279cc7384
Sherif-toiema22 <sherif.toiema22@gmail.com>
2025-08-24 00:57:01 +0300

    Just Start

---
+++ src/main/java/com/sherif/Employees/System/EmployeesSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sherif.Employees.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesSystemApplication.class, args);
+	}
+
+}
